ff63f8dcf24407f391c17dfb9d478ad00cad8643
姚奕廷 <ting20121108@gmail.com>
2020-05-15 14:12:21 +0800

    更改文字

--- app/src/main/java/tw/edu/pu/s1071463/my0515/MainActivity.java
+++ app/src/main/java/tw/edu/pu/s1071463/my0515/MainActivity.java
@@ -3,12 +3,15 @@ package tw.edu.pu.s1071463.my0515;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txv = findViewById(R.id.txv);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txv.setText("Hello hello");
     }
 }
